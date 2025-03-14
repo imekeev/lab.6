8a52daf681fc6087932a860cbc1b0fc8ac3ed41a
imekeev <imekeev@icloud.com>
2025-03-15 00:41:26 +0500

    Update 15.py

--- 15.py
+++ 15.py
@@ -1,6 +1,6 @@
 path = input()
 l = [1, 2, 3, 4, 5, 6, 7]
 
-with open(path, 'w') as file:
+with open(path, 'w') as file: //открываем файл на запись данных чисел ))
     for x in l:
-        file.write(str(x) + '\n')
+        file.write(str(x) + '\n') //каждое число с новой строки:)
