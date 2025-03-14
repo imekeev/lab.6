e0688883138d557d5c91ac1d133f8a6a27d89d0e
imekeev <imekeev@icloud.com>
2025-03-15 00:33:12 +0500

    Update 12.py

--- 12.py
+++ 12.py
@@ -1,7 +1,7 @@
 import os
 
 path = input()
-print(os.path.exists(path))
+print(os.path.exists(path)) //проверяем путь к папке
 print(os.access(path, os.R_OK)) //читать
 print(os.access(path, os.W_OK)) //писать
 print(os.access(path, os.X_OK)) //запускать
