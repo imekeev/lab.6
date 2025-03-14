e6a27f4d254f8d0a2acf057036649f8ac5230c04
imekeev <imekeev@icloud.com>
2025-03-15 00:37:23 +0500

    Update 14.py

--- 14.py
+++ 14.py
@@ -1,7 +1,7 @@
 path = input()
 count = 0
-with open(path, "r") as file:
-    for line in file:
+with open(path, "r") as file: //открываем файл на чтение 
+    for line in file: //проходимся по каждой строке и ведем подсчет
         count += 1
 
 print(count)
