524c8a01f1c9566a5b4b8f32c69ec8cf03052e80
imekeev <imekeev@icloud.com>
2025-03-15 00:44:26 +0500

    Update 16.py

--- 16.py
+++ 16.py
@@ -1,6 +1,6 @@
 import string
 
-for letter in string.ascii_uppercase:
-    file_name = f"{letter}.txt"
+for letter in string.ascii_uppercase: //проходимся по всем заглавынм буквам алфавита
+    file_name = f"{letter}.txt" //создаем имя файла начиная с A.txt
     with open(file_name, 'w') as file:
-        file.write(f"This is {file_name}")
+        file.write(f"This is {file_name}") //открываем файл на запись и записываем 
