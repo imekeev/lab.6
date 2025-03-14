554bfd65641759692e86f21ca859977285bb7eae
imekeev <imekeev@icloud.com>
2025-03-15 00:32:27 +0500

    12.py

--- 12.py
+++ 12.py
@@ -2,6 +2,6 @@ import os
 
 path = input()
 print(os.path.exists(path))
-print(os.access(path, os.R_OK))
-print(os.access(path, os.W_OK))
-print(os.access(path, os.X_OK))
+print(os.access(path, os.R_OK)) //читать
+print(os.access(path, os.W_OK)) //писать
+print(os.access(path, os.X_OK)) //запускать
