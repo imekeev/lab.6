7f3a4844afafc53d4b1a2e49b621029637f5be0e
imekeev <imekeev@icloud.com>
2025-03-15 00:46:17 +0500

    Update 17.py

--- 17.py
+++ 17.py
@@ -1,7 +1,7 @@
 path = input()
 
 with open(path, "r") as file:
-    content = file.read()
+    content = file.read() //открываем файл на чтение
 
 with open("copy.txt", "w") as file:
-    file.write(content)
+    file.write(content) //копируем контент и вставляем в новый файл 
